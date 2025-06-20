d300fde4b4e893a735b36d5087a7e1428ebfc588
Hendry Widyanto <hendrywidyanto97@gmail.com>
2025-06-20 18:18:56 +0700

    Trial

--- src/constants.ts
+++ src/constants.ts
@@ -3,8 +3,8 @@ import Env from '@/lib/env';
 // Config
 export const PACKAGE = Env.PACKAGE;
 export const EAS_PROJECT_ID = Env.EAS_PROJECT_ID;
-// export const API_BASE_URL = Env.API_URL;
-export const API_BASE_URL = 'http://192.168.100.225:3000';
+export const API_BASE_URL = Env.API_URL;
+// export const API_BASE_URL = 'http://192.168.100.225:3000';
 
 
 
